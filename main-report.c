b6309cec1ac210138607bf0322090be8b327b704
byte[] <byteslice@airmail.cc>
2021-02-27 00:16:11 -0500

    add core mix to reporting

--- main-report.c
+++ main-report.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <fcntl.h>
 #include <signal.h>
 #include <stdlib.h>
@@ -14,16 +16,54 @@
 // Energy monitoring
 static int voltage_dev_fd = -1;
 static int current_dev_fd = -1;
+static int proc_stat_fd = -1;
 
 static const char *voltage_dev = "/sys/class/power_supply/bms/voltage_now";
 static const char *current_dev = "/sys/class/power_supply/bms/current_now";
 
 static int64_t voltage_sum = 0;
 static int64_t current_sum = 0;
+static int core_num = 0;
 static int64_t num_samples = 0;
 
 static struct timespec start_time;
 static struct timespec end_time;
+
+static void setup_proc_stat(pid_t pid)
+{
+	char *filename;
+	asprintf(&filename, "/proc/%d/stat", pid);
+
+	proc_stat_fd = open(filename, O_RDONLY);
+	if (proc_stat_fd < 0) die("open stat");
+
+	free(filename);
+}
+
+static void read_proc_stat()
+{
+	char buf[800];
+	int proc;
+
+	lseek(proc_stat_fd, 0, SEEK_SET);
+	if (read(proc_stat_fd, buf, sizeof(buf)-1) < 0) die("read stat");
+
+	sscanf(buf,
+		"%*d %*s "		// pid, comm
+		"%*c "			// state
+		"%*d %*d %*d %*d %*d"	// ppid, pgrp, session, tty_nr, tpgid
+		"%*u %*u %*u %*u"	// minflt, cminflt, majflt, cmajflt
+		"%*u %*u %*d %*d"	// utime, stime, cutime, cstime
+		"%*d %*d %*d %*d"	// priority, nice, num_threads, itrealvalue
+		"%*u %*u %*d %*u"	// starttime, vsize, rss, rsslim
+		"%*u %*u %*u %*u"	// startcode, endcode, startstack, kstkesp
+		"%*u %*u %*u %*u"	// kstkeip, signal, blocked, sigignore
+		"%*u %*u %*u %*u"	// sigcatch, wchan, nswap, cnswap
+		"%*d %d",		// exit_signal, processor.
+		&proc);
+
+	core_num += (proc >= 6);
+}
 
 static void setup_energy_monitor()
 {
@@ -46,8 +86,6 @@ static void read_power()
 
 	if (read(current_dev_fd, buf, sizeof(buf)) < 0) die("read current");
 	current_sum += atol(buf) / 1000;
-
-	num_samples++;
 }
 
 static void report_energy()
@@ -67,10 +105,18 @@ static void report_energy()
 	printf("%ld ms\n", elapsed_time);
 }
 
+static void report_core_mix()
+{
+	float mix = core_num / (float) num_samples;
+
+	printf("%f big, %f LITTLE\n", mix, 1 - mix);
+}
+
 static void sigchld_handler(int signal)
 {
 	clock_gettime(CLOCK_MONOTONIC_RAW, &end_time);
 	report_energy();
+	report_core_mix();
 
 	exit(0);
 }
@@ -94,8 +140,12 @@ int main(int argc, char *argv[], char *envp[])
 		die("execve");
 	}
 
+	setup_proc_stat(child);
+
 	while (1) {
 		read_power();
+		read_proc_stat();
+		num_samples++;
 		usleep(200000);
 	}
 
