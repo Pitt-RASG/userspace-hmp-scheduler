aa01ab2ac2d3d5018557cd1646677fed1b9d9df7
byte[] <byteslice@airmail.cc>
2021-03-02 23:24:36 -0500

    python FFI

--- predictor.py
+++ predictor.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python3
 import sys
-import numpy as np 
+import numpy as np
+from cffi import FFI
 from pickle import load
 
 def main():
+    ffi = FFI()
+    ffi.cdef("""
+    typedef int (*predict_phase)(long, long, long, long, long, int);
+    int scheduler_main(char *argv[], predict_phase cb);
+    """)
+    lib = ffi.dlopen("./libschedule.so")
+
     model = load(open("phases-model.pkl", "rb"))
     scaler = load(open("phases-scaler.pkl", "rb"))
-    flag = True
-    while flag:
-        try:
-            pmc1, pmc2, pmc3, pmc4, pmc5, cluster = input().split(',') 
-            p1 = int(pmc1)
-            p2 = int(pmc2)
-            p3 = int(pmc3)
-            p4 = int(pmc4)
-            p5 = int(pmc5)
-            cluster = int(cluster)
-            pmcs = [[p3, p5, p1, p4, p2, cluster]] # order is based on how the ML was trained
 
-            scaled_pmcs = scaler.transform(pmcs)
+    @ffi.callback("int(long,long,long,long,long,int)")
+    def run_prediction(pmc1, pmc2, pmc3, pmc4, pmc5, cluster):
+        pmcs = [[p3, p5, p1, p4, p2, cluster]] # order is based on how the ML was trained
+        scaled_pmcs = scaler.transform(pmcs)
+        return model.predict(scaled_pmcs)[0]
 
-            print(model.predict(scaled_pmcs)[0])
-            
-        except EOFError:
-            flag = False
+    lib.scheduler_main([ffi.new("char[]", x) for arg in sys.argv[2:]], run_prediction)
 
 if __name__ == "__main__":
     main()
